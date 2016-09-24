6bedd64408164c1f1229bf6faf37409a89f66c4e
shindevaibhav <shinde.vaibhav1793@gmail.com>
2016-09-23 22:00:45 -0400

    added code to make the default root admin

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
   has_many :bookings
 
-  validates :room_number, :presence=>true
+  validates :room_number, :presence=>true, :uniqueness => true
   validates :size, :presence=>true
   validates :building, :presence=>true
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#Creating a default admin
+#TODO
+# execute "INSERT INTO admins (name,email,password_digest) VALUES ('LibSystem Root','root@libSys.com', '$2a$10$bxnGljeITdh1vjpt944fD.0nGxUsirQ/S.CuG3aSiHnik8dGSlU1K');"
+
+root = Admin.new({:name =>"LibSystem Root", :email => "root@libsys.com",  :password => "root_123", :password_confirmation => "root_123" })
+
+root.save!
